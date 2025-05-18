66531e3ab0a03a03503033b4b191e50bd9878173
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-18 20:30:41 +0200

    refactor winning condition

--- S4-tic-tac-toe/src/App.jsx
+++ S4-tic-tac-toe/src/App.jsx
@@ -2,8 +2,8 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import Player from "./components/Player";
 import { useState } from "react";
-import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
+import { determineWinner } from "./determineWinner";
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
@@ -16,29 +16,8 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  // WINNING CONDITIONS
-  let winner = null;
-  for (const combination of WINNING_COMBINATIONS) {
-    const [first, second, third] = combination;
-    const firstTurn = gameTurns.find(
-      (turn) => turn.square.row === first.row && turn.square.col === first.col
-    );
-    const secondTurn = gameTurns.find(
-      (turn) => turn.square.row === second.row && turn.square.col === second.col
-    );
-    const thirdTurn = gameTurns.find(
-      (turn) => turn.square.row === third.row && turn.square.col === third.col
-    );
-
-    if (
-      firstTurn?.player &&
-      firstTurn.player === secondTurn?.player &&
-      firstTurn.player === thirdTurn?.player
-    ) {
-      winner = firstTurn.player;
-      break;
-    }
-  }
+  // WINNING CONDITION
+  let winner = determineWinner(gameTurns);
   // draw condition: if all squares are filled and no winner, it's a draw
   const draw = gameTurns.length === 9 && !winner;
 
@@ -53,6 +32,10 @@ function App() {
     });
   }
 
+  function handlePlayAgain() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -60,7 +43,7 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        {winner || draw ? <GameOver winner={winner} /> : null}
+        {winner || draw ? <GameOver winner={winner} onPlayAgain={handlePlayAgain} /> : null}
         <GameBoard
           activePlayer={activePlayer}
           onSelectTile={handleSelectTile}
