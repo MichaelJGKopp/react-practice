e4f66dc4be83b6150038c7207ff390c6000fddd6
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-18 13:03:25 +0200

    add Tabs.jsx

---
+++ S3-react-essentials/src/components/Tabs.jsx
@@ -0,0 +1,13 @@
+// This component is a simple wrapper for the tabs functionality.
+// Set multiple slots for the buttons and the content.
+export default function Tabs({children, buttons, ButtonsContainer = "menu" }) {
+    // const ButtonsContainer = buttonsContainer;
+  return (
+    <>
+      <ButtonsContainer>
+        {buttons}
+      </ButtonsContainer>
+      {children}
+    </>
+  );
+}
