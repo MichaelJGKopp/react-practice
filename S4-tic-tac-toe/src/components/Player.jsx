a433c0cab26ce9bf38b8671e3763630623f8ccbf
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-18 14:44:59 +0200

    add Player Component with changeable name

---
+++ S4-tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((prev) => !prev);  // best practice to user a function to update state
+  };
+
+  function handleChange(event) {    // by default the handleChange event provides the event object
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            value={playerName}
+            required
+            onChange={handleChange}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
