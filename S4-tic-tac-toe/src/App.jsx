b83b3ea7d7ed440ef4fa0faa259e8d44ff7a7cce
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-18 18:18:28 +0200

    select tile on board, lift up activePlayer

--- S4-tic-tac-toe/src/App.jsx
+++ S4-tic-tac-toe/src/App.jsx
@@ -1,16 +1,23 @@
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectTile() {
+    setActivePlayer((prev) => (prev === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
 
-        <GameBoard />
+        <GameBoard activePlayer={activePlayer} onSelectTile={handleSelectTile} />
       </div>
 
       LOG
--- S4-tic-tac-toe/src/components/GameBoard.jsx
+++ S4-tic-tac-toe/src/components/GameBoard.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({
+  activePlayer = "X",
+  onSelectTile,
+}) {
+  const [currentGameBoard, setCurrentGameBoard] = useState(initialGameBoard);
+
+  function handleCellClick(currentRowIndex, currentColIndex) {
+    setCurrentGameBoard((prevGameBoard) => {
+      // Create a deep copy of the game board for immutability when updating the state
+      const updatedBoard = [...prevGameBoard.map((row) => [...row])];
+      updatedBoard[currentRowIndex][currentColIndex] = activePlayer;
+      return updatedBoard;
+    });
+
+    onSelectTile(); // switch the active player
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {currentGameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleCellClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
