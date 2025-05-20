3f95cc7db1f6b4577062e3ab285fc18454deffe8
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-20 21:18:15 +0200

    styled components

--- S6-styling-react/src/components/AuthInputs.jsx
+++ S6-styling-react/src/components/AuthInputs.jsx
@@ -1,12 +1,45 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { styled } from "styled-components";
+
+// template literals
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({invalid})=> invalid ? '#ef4444' : '#6b7280'};
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: ${({invalid})=> invalid ? '#ef4444' : '#374151'};
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  border-color: ${({invalid})=> invalid ? '#ef4444' : undefined};
+  background-color: ${({invalid})=> invalid ? '#fed2d2' : undefined};
+  transition: all 0.5s;
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -17,15 +50,20 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p>
-          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
-          <input
+          <Label 
+          $invalid={emailNotValid}> 
+          {/* className={`label ${emailNotValid ? "invalid" : ""}`}> */}
+            Email
+          </Label>
+          <Input
+            $invalid={emailNotValid}
             type="email"
             /* inline styles are not recommended
             style={{
@@ -33,26 +71,30 @@ export default function AuthInputs() {
               borderColor: emailNotValid ? '#ef4444' : undefined,
               backgroundColor: emailNotValid ? '#fed2d2' : undefined,
             }} */
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            // className={emailNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</label>
-          <input
+          <Label className={`label ${passwordNotValid ? "invalid" : ""}`}>
+            Password
+          </Label>
+          <Input
             type="password"
             // className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
