4ca630f5568ba874039f36f7e1b52933e746ca76
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-24 16:31:48 +0200

    init time challenge with useRef example

---
+++ S8-time-challenge/src/components/Player.jsx
@@ -0,0 +1,27 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setPlayerName] = useState(null);
+
+  function handleSubmit() {
+    setPlayerName(playerName.current.value);
+    playerName.current.value = ""; // !! imperative code 
+    // leave manipulating the DOM generally to React
+    // but this is a common pattern to clear the input field
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <p>
+        <input  // rendered here the first time, 
+        // ref playerName doesn't exist before in 1st render cycle
+          ref={playerName}  // !!
+          type="text"
+        />
+        <button onClick={handleSubmit}>Set Name</button>
+      </p>
+    </section>
+  );
+}
