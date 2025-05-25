d4bca24905b059b683e65c0469b31149e86fc447
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-25 18:57:06 +0200

    add result modal

---
+++ S8-time-challenge/src/components/ResultModal.jsx
@@ -0,0 +1,13 @@
+export default function ResultModal({ result, targetTime }) {
+  return (
+    <dialog className="result-modal" open>
+      <h2>You {result}</h2>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+}
--- S8-time-challenge/src/components/TimerChallenge.jsx
+++ S8-time-challenge/src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
 
 // let timer; // doesn't work, shared across all instances of the component
 
@@ -21,20 +22,22 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>"You lost!"</p>}
-      <p className="challenge-time">
-        {targetTime} seconds{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? "Stop Challenge" : "Start Challenge"}
-        </button>
-      </p>
-      <p className={timerStarted ? "active" : undefined}>
-        {timerStarted ? "Time is running..." : "Timer inactive"}
-      </p>
-    </section>
+    <>
+      {timerExpired && <ResultModal result="lost" targetTime={targetTime} />}
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} seconds{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop Challenge" : "Start Challenge"}
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Time is running..." : "Timer inactive"}
+        </p>
+      </section>
+    </>
   );
 }
